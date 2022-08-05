26ff75339676865db7f38ac171a94ce3fa8287bf
jogindrakumar <joginder9868@outlook.com>
2022-08-05 16:47:35 +0530

    modified:   public/frontend/css/style.css 	modified:   resources/views/frontend/body/social-media.blade.php

--- resources/views/frontend/body/social-media.blade.php
+++ resources/views/frontend/body/social-media.blade.php
@@ -3,4 +3,5 @@
     <i class="fab fa-instagram pe-3 text-warning" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Not Available"></i>
     <i class="fab fa-facebook-f pe-3 text-primary" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Not Available"></i>
     <i class="fab fa-youtube text-danger" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Not Available"></i>
+    <i class="fas fa-shopping-cart text-warning ms-2" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Not Available"></i>
 </div>
