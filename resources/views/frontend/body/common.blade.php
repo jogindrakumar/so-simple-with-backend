0e1a939424a2b7a579516e25d43ad28be23c0267
jogindrakumar <joginder9868@outlook.com>
2022-08-16 07:41:05 +0530

    new file:   public/upload/project/1741225087042030.png 	modified:   resources/views/frontend/body/common.blade.php

--- resources/views/frontend/body/common.blade.php
+++ resources/views/frontend/body/common.blade.php
@@ -6,7 +6,7 @@
                     <img src="{{ asset('frontend/img/me.jfif') }}" class="card-img-top position-absolute top-0 start-50 translate-middle border border-primary" alt="...">
                 </div>
                 <div class="card-body mt-4">
-                    <h5 class="card-title"><b>@yield('card-title')</b> </h5>
+                    <h5 class="card-title" style="font-size: 1.5rem;"><b>@yield('card-title')</b> </h5>
                     <h6 class="card-subtitle mb-2 text-muted">@yield('card-subtitle')</h6>
                     <p class="card-text">@yield('card-text')</p>
 
